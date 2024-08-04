1860852f10ef79ee199315f47370ea9d7669b275
Mehdi Chikhi <ci_mi@outlook.fr>
2024-08-04 09:07:15 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mehdi on 4/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
